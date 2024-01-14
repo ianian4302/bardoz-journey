74d32a55d8a83f0a04c21765f37c6f0f3e54f1f3
ianian4302 <88609092+ianian4302@users.noreply.github.com>
2024-01-14 23:43:15 +0800

    docs

--- include/var.h
+++ include/var.h
@@ -3,8 +3,8 @@
 // Global variables
 
 // Window size
-const int screenWidth = 1600;
-const int screenHeight = 900;
+const int screenWidth = 1920;
+const int screenHeight = 1200;
 
 // Frames per second
 const int framesPerSecond = 90;
@@ -15,3 +15,6 @@ const int buttnonTitleSize = 40;
 // Text settings
 const int iconScale = 2;
 const int fontSize = 40;
+
+// Physical properties
+const float acceleration = 0.1f;
